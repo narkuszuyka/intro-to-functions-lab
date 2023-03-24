1de88b6294c1508b0f5c32522e063972b730036c
Sukran <sukran.akyuz.93@gmail.com>
2023-03-24 17:12:38 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,40 @@
+
+
+function shout(string) {
+    return string.toUpperCase();
+  }
+  
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+  
+  function logShout(string) {
+
+    
+    console.log(string.toUpperCase());
+  }
+
+
+  function logWhisper(string) {
+
+    
+    console.log(string.toLowerCase());
+  }
+
+  function sayHiToHeadphonedRoommate(string) {
+
+if (string.toLowerCase(string) === (string)) 
+
+  return "I can't hear you!";
+
+if (string.toUpperCase(string) === (string)) 
+ 
+  return "YES INDEED!"
+
+if ((string) === "Let's have dinner together!" )
+
+return "I would love to!"
+
+  }
+
+
